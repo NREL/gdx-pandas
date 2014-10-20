67f4e56acfab98b11122d4c5b53f7d48409645e2
Elaine Hale <elaine.hale@nrel.gov>
2014-10-20 16:22:15 -0600

    Translation of command line interfaces.

---
+++ bin/csv_to_gdx.py
@@ -0,0 +1,59 @@
+import pandas as pds
+import gdxpds
+
+import argparse
+import os
+
+def convert_csv_to_gdx(input_files, output_file, gams_dir=None):
+    # check input files
+    for ifile in input_files:
+        if not os.path.splitext(ifile)[1] in ['.csv','.txt']:
+            msg = "Input file '{}' is of unexpected type. Expected .csv or .txt.".format(ifile)
+            raise RuntimeError(msg)
+        if not os.path.isfile(ifile):
+            raise RuntimeError("'{}' is not a file.".format(ifile))
+   
+    # convert input_files into one list of csvs
+    ifiles = []
+    for ifile in input_files:
+        if os.path.splitext(ifile)[1] == '.csv':
+            ifiles.append(ifile)
+        else:
+            # must be .txt
+            f = open(ifile, 'r')
+            for line in f:
+                if not line == '':
+                    if os.path.splitext(line)[1] == '.csv':
+                        ifiles.append(line)
+                    else:
+                        print("Skipping '{}' found in '{}'.".format(line,ifile))
+            f.close()
+    if len(ifiles) == 0:
+        raise RuntimeError("Nothing to convert.")
+        
+    # convert list of csvs to map of dataframes
+    dataframes = {}
+    for ifile in ifiles:
+        dataframes[os.path.splitext(os.path.basename(ifile))[0]] = 
+            pds.DataFrame.from_csv(ifile, index_col = None)
+            
+    gdxpds.to_gdx(dataframes, output_file, gams_dir)
+    
+
+if __name__ == "__main__":
+
+    # define and execute the command line interface
+    parser = argparse.ArgumentParser(description='''Accepts one or more input
+        csv files as input. Writes each csv as a separate symbol to an output
+        gdx.''')
+    parser.add_argument('-i', '--input', nargs='+', help='''List one or more
+        .csv or .txt files. The latter are assumed to be a line-delimited list
+        of .csv files.''')
+    parser.add_argument('-o', '--output', default='export.gdx', help='''Path
+        to the output gdx file. Will be overwritten if it already exists.''')
+    parser.add_argument('-g', '--gams_dir', help='''Path to GAMS installation
+        directory.''', default = None)
+        
+    args = parser.parse_args()
+    
+    convert_csv_to_gdx(args.input, args.output, args.gams_dir)
---
+++ bin/gdx_to_csv.py
@@ -0,0 +1,38 @@
+import gdxpds
+
+import argparse
+import os
+
+def convert_gdx_to_csv(in_gdx, out_dir, gams_dir=None):
+    # check inputs
+    if not os.path.exists(os.path.dirname(out_dir)):
+        raise RuntimeError("Parent directory of output directory '{}' does not exist.".format(out_dir))
+        
+    # convert to pandas.DataFrames
+    dataframes = gdxpds.to_dataframes(in_gdx, gams_dir)
+
+    # write to files
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+    
+    for symbol_name, df in dataframes:
+        csv_path = os.path.join(out_dir, symbol_name + ".csv")
+        if os.path.exists(csv_path):
+            print("Overwriting '{}'".format(csv_path))
+        df.to_csv(csv_path,index=False)
+
+if __name__ == "__main__":
+
+    # define and execute the command line interface
+    parser = argparse.ArgumentParser(description='''Reads a gdx file into
+        pandas dataframes, and then writes them out as csv files.''')
+    parser.add_argument('-i', '--in_gdx', help='''Input gdx file to be read
+                        and exported as one csv per symbol.''')
+    parser.add_argument('-o', '--out_dir', default='./gdx_data/',
+                        help='''Directory to which csvs are to be written.''')
+    parser.add_argument('-g', '--gams_dir', help='''Path to GAMS installation
+                        directory.''', default = None)
+        
+    args = parser.parse_args()
+    
+    convert_gdx_to_csv(args.in_gdx, os.path.realpath(args.out_dir), args.gams_dir)
