b7ad2e6eb5b2ad5bc111aa056dfa3afaf34c5906
Elaine Hale <elaine.hale@nrel.gov>
2014-10-21 10:56:44 -0600

    Adding to package.

--- setup.py
+++ setup.py
@@ -7,8 +7,13 @@ setup(
     version = gdxpds.__version__,
     author = 'Elaine T. Hale',
     author_email = 'elaine.hale@nrel.gov',
-    packages = ['gdxpds'],
+    packages = ['gdxpds', 'gdxpds.test'],
+    scripts = ['bin/csv_to_gdx.py', 'bin/gdx_to_csv.py']
     url = 'https://github.nrel.gov/ehale/gdx-pandas',
     description = 'Python package to translate between gdx (GAMS data) and pandas.',
+    package_data={
+        'gdxpds.test': ['gdxpds/test/*.csv',
+                        'gdxpds/test/*.gdx']
+    },
     install_requires=open('requirements.txt').read()
 )
