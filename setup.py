389fc82ff9726d06d775761eee11c6e069c56325
Elaine Hale <elaine.hale@nrel.gov>
2014-10-21 11:24:24 -0600

    Forgot a comma.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author = 'Elaine T. Hale',
     author_email = 'elaine.hale@nrel.gov',
     packages = ['gdxpds', 'gdxpds.test'],
-    scripts = ['bin/csv_to_gdx.py', 'bin/gdx_to_csv.py']
+    scripts = ['bin/csv_to_gdx.py', 'bin/gdx_to_csv.py'],
     url = 'https://github.nrel.gov/ehale/gdx-pandas',
     description = 'Python package to translate between gdx (GAMS data) and pandas.',
     package_data={
