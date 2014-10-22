4b01c65dd5615a31acfc6d50e5186e29c5056762
Elaine Hale <elaine.hale@nrel.gov>
2014-10-22 10:16:55 -0600

    Adding gdxpds.test.bin_prefix for installed package.

--- gdxpds/test/__init__.py
+++ gdxpds/test/__init__.py
@@ -3,7 +3,24 @@ import os
 # if True, test products will be deleted on tear down
 clean_up = True
 
-bin_prefix = ''
-candidate = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'bin')
+
+def apply_dirname(f, num_times):
+    ret = f
+    for i in range(num_times):
+        ret = os.path.dirname(ret)
+    return ret
+
+bin_prefix = ''    
+# git repo (development)
+candidate = os.path.join(apply_dirname(__file__,3), 'bin')
 if os.path.isdir(candidate) and os.path.isfile(os.path.join(candidate, 'gdx_to_csv.py')):
     bin_prefix = candidate
+
+# install location for Windows
+candidate = os.path.join(apply_dirname(__file__,4), 'Scripts')
+if bin_prefix == '' and os.path.isdir(candidate) and os.path.isfile(os.path.join(candidate, 'gdx_to_csv.py')):
+    bin_prefix = candidate
+
+# TODO: install location for Mac
+
+# TODO: install location for Linux
