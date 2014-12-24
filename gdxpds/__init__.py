520e5bb43aa3b9a4cdd309f185861e6fc98ea342
Elaine Hale <elaine.hale@nrel.gov>
2014-12-24 12:58:15 -0700

    Just get one symbol. Requires version of py-gdx with lazy load.

--- gdxpds/__init__.py
+++ gdxpds/__init__.py
@@ -2,4 +2,6 @@
 __version__ = '0.2.0'
 
 from gdxpds.read_gdx import to_dataframes
+from gdxpds.read_gdx import list_symbols
+from gdxpds.read_gdx import to_dataframe
 from gdxpds.write_gdx import to_gdx
--- gdxpds/read_gdx.py
+++ gdxpds/read_gdx.py
@@ -4,8 +4,8 @@ import pandas as pds
 import gdxpds.tools
 
 class Translator(object):
-    def __init__(self, gdx_file, gams_dir = None):
-        self.__gdx_loader = gdxpds.tools.GdxLoader(gdx_file, gams_dir)
+    def __init__(self, gdx_file, gams_dir = None, lazy_load = False):
+        self.__gdx_loader = gdxpds.tools.GdxLoader(gdx_file, gams_dir, lazy_load)
         self.__dataframes = None
         
     @property
@@ -34,28 +34,48 @@ class Translator(object):
         if self.__dataframes is None:
             self.__translate()
         return self.__dataframes
+        
+    @property
+    def symbols(self):
+        return [symbol_name for symbol_name in self.gdx]
+        
+    def dataframe(self, symbol_name):
+        if not symbol_name in self.symbols:
+            raise RuntimeError("No symbol named '{}' in '{}'.".format(symbol_name, self.gdx_file))
+        if (self.__dataframes is None) or (not symbol_name in self.__dataframes):
+            self.__translate(symbol_name)
+        return { symbol_name: self.__dataframes[symbol_name] }
                 
-    def __translate(self):    
+    def __translate(self, symbol_name = None):    
         # recursive helper function
         def collect_data(data, entry, dim):
             assert isinstance(dim, gdxdict.gdxdim)
-            for key, value in dim.items.items():
-                key_name = self.gdx.order[self.gdx.universal[key]]
+            # cannot use dim.items.items() here, because skips the lazy_load feature
+            for key in dim:
+                key_name = self.gdx.universal[key.lower()]['name']
+                value = dim[key]
                 if isinstance(value,gdxdict.gdxdim):
                     collect_data(data,entry + [key_name],value)
                 else:
                     data.append(entry + [key_name, value])
         
-        # main body        
-        assert self.__dataframes is None
-        self.__dataframes = {}
-        for symbol_name in self.gdx:
+        # main body   
+        if self.__dataframes is None:
+            self.__dataframes = {}
+
+        symbol_names = None
+        if symbol_name is None:
+            symbol_names = self.symbols
+        else:
+            symbol_names = [symbol_name]
+            
+        for symbol_name in symbol_names:
             symbol_info = self.gdx.getinfo(symbol_name)
             if symbol_info['records'] > 0:
-                cols = [d['key'] for d in symbol_info["domain"]]
-                cols.append('value')
                 data = []; entry = []
                 collect_data(data,entry,self.gdx[symbol_name])
+                cols = [d['key'] for d in symbol_info["domain"]]
+                cols.append('value')
                 self.__dataframes[symbol_name] = pds.DataFrame(data = data, columns = cols)
 
 def to_dataframes(gdx_file, gams_dir = None):
@@ -63,11 +83,36 @@ def to_dataframes(gdx_file, gams_dir = None):
     Primary interface for converting a GAMS GDX file to pandas DataFrames.
     
     Parameters:
-      - gdxfile (string): path to a GDX file
-      - gamsdir (string): optional path to GAMS directory
+      - gdx_file (string): path to a GDX file
+      - gams_dir (string): optional path to GAMS directory
       
     Returns a dict of Pandas DataFrames, one item for each symbol in the GDX 
     file, keyed with the symbol name.
     """
     return Translator(gdx_file, gams_dir).dataframes
+    
+def list_symbols(gdx_file, gams_dir = None):
+    """
+    Returns the list of symbols available in gdx_file.
+    
+    Parameters:
+      - gdx_file (string): path to a GDX file
+      - gams_dir (string): optional path to GAMS directory      
+    """
+    return Translator(gdx_file, gams_dir, lazy_load = True).symbols
+    
+def to_dataframe(gdx_file, symbol_name, gams_dir = None):
+    """
+    Interface for getting the { symbol_name: pandas.DataFrame } dict for a 
+    single symbol.
+    
+    Parameters:
+      - gdx_file (string): path to a GDX file
+      - symbol_name (string): symbol whose pandas.DataFrame is being requested
+      - gams_dir (string): optional path to GAMS directory    
+      
+    Returns a dict with a single entry, where the key is symbol_name and the
+    value is the corresponding pandas.DataFrame.
+    """
+    return Translator(gdx_file, gams_dir, lazy_load = True).dataframe(symbol_name)
     
